72bd331eb40b76bd70441bf962c7aa4db4018757
yasufumi <yasufumi.taniguchi@gmail.com>
2018-08-06 10:17:07 +0900

    update FastQA model in models.py

--- models.py
+++ models.py
@@ -1,10 +1,9 @@
-import tensorflow as tf
 from keras import backend as K
 from keras import Model
-from keras.layers import Input, Embedding, LSTM, Dense, Concatenate, Lambda, \
-    Bidirectional
+from keras.layers import Input, Embedding, LSTM, Dense, Concatenate, \
+    Bidirectional, RepeatVector
 
-from layers import WeightedSum, WordInQuestionB, WordInQuestionW
+from layers import WeightedSum, WordInQuestionB, WordInQuestionW, PositionPointer, IndexSelect
 
 
 class FastQA:
@@ -14,35 +13,38 @@ class FastQA:
         self.context_lstm = Bidirectional(LSTM(hidden_size, return_sequences=True))
         self.question_fc = Dense(hidden_size)
         self.context_fc = Dense(hidden_size)
-        self.answer_start_fc = Dense(hidden_size)
-        self.start_fc = Dense(1)
-        self.answer_end_fc = Dense(hidden_size)
-        self.end_fc = Dense(1)
+        self.start_pointer = PositionPointer(hidden_size)
+        self.end_pointer = PositionPointer(hidden_size)
 
     def build(self):
         question_input = Input((None,))
         context_input = Input((None,))
         start_input = Input((None,))
 
+        # question
         Q = self.embed_layer(question_input)
+        Q_wiqb = WordInQuestionB()([question_input, question_input])
+        Q_wiqw = WordInQuestionW()([Q, Q])
+        Q = Concatenate()([Q_wiqb, Q_wiqw, Q])
+        Z = self.question_lstm(Q)
+        Z = self.question_fc(Z)
+        # context
         X = self.embed_layer(context_input)
         X_wiqb = WordInQuestionB()([question_input, context_input])
         X_wiqw = WordInQuestionW()([Q, X])
         X = Concatenate()([X_wiqb, X_wiqw, X])
-        Q_wiqb = WordInQuestionB()([question_input, question_input])
-        Q_wiqw = WordInQuestionW()([Q, Q])
-        Q = Concatenate()([Q_wiqb, Q_wiqw, Q])
+        H = self.context_lstm(X)
+        H = self.context_fc(H)
 
-        Z = self.question_lstm(Q)
-        Z = self.question_fc(Z)
         z = WeightedSum()(Z)
         context_len = K.int_shape(context_input)[1]
-        Z = Lambda(lambda x: tf.tile(x, [1, context_len, 1]))(z)
+        # (batch, seq_eln, hidden_size)
+        Z = RepeatVector(context_len)(z)
 
-        H_dash = self.context_lstm(X)
-        H = self.context_fc(H_dash)
+        start_output = self.start_pointer(Concatenate([H, Z, H * Z]))
+        h_s = IndexSelect()([H, start_input])
+        H_s = RepeatVector(context_len)(h_s)
+        end_output = self.end_pointer(Concatenate([H, H_s, H * Z, H * H_s]))
 
-        start = self.start_fc(self.answer_start_fc(Concatenate()([H, Z, H * Z])))
-        end = self.end_fc(self.answer_end_fc(Concatenate()([H, Z, H * Z])))
-
-        return Model(inputs=[question_input, context_input, start_input], outputs=[start, end])
+        return Model(inputs=[question_input, context_input, start_input],
+                     outputs=[start_output, end_output])
