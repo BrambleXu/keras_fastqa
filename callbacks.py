4a965df04c8d758483ad45db54b70c0e36709a51
yasufumi <yasufumi.taniguchi@gmail.com>
2018-08-08 16:24:49 +0900

    add LearningRateScheduler in callbacks.py

---
+++ callbacks.py
@@ -0,0 +1,16 @@
+from keras import backend as K
+from keras.callbacks import Callback
+
+
+class LearningRateScheduler(Callback):
+    def __init__(self):
+        self.global_steps = 1
+
+    def on_batch_begin(self, batch, logs=None):
+        if self.global_steps % 1000 == 0:
+            lr = float(K.get_value(self.model.optimizer.lr))
+            lr /= 2
+            K.set_value(self.model.optimizer.lr, lr)
+            print(f'\nTraining steps {self.global_steps}: LearningRateScheduler setting learning '
+                  'rate to {lr}')
+        self.global_steps += 1
