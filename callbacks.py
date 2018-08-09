ac286dc5d2c606dfbf90a18c7e850986480246b4
yasufumi <yasufumi.taniguchi@gmail.com>
2018-08-09 09:46:23 +0900

    fix bug in FastQALRScheduler

--- callbacks.py
+++ callbacks.py
@@ -3,14 +3,15 @@ from keras.callbacks import Callback
 
 
 class FastQALRScheduler(Callback):
-    def __init__(self, val_generator):
+    def __init__(self, val_generator, steps=1000):
         self.val_generator = val_generator
         self.val_steps = len(val_generator)
         self.global_steps = 1
+        self.steps = 1000
         self.last_val_loss = float('inf')
 
     def on_batch_end(self, batch, logs=None):
-        if self.global_steps % 1 == 0:
+        if self.global_steps % self.steps == 0:
             val_loss = self.model.evaluate_generator(
                 self.val_generator, steps=self.val_steps, workers=0)[0]
             if self.last_val_loss < val_loss:
