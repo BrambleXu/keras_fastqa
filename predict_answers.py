f14c8bde7065c97becf80a0b2ce7b7c75e7f736a
yasufumi <yasufumi.taniguchi@gmail.com>
2018-08-16 11:05:12 +0900

    add predict_answers.py

---
+++ predict_answers.py
@@ -0,0 +1,47 @@
+import json
+from argparse import ArgumentParser
+
+from models import FastQA
+from data import SquadReader, Iterator, Vocabulary, SquadEvalConverter, get_tokenizer
+
+from prepare_vocab import PAD_TOKEN, UNK_TOKEN
+
+
+def main(args):
+    token_to_index, index_to_token = Vocabulary.load(args.vocab_file)
+
+    model = FastQA(len(token_to_index), args.embed, args.hidden,
+                   question_limit=args.q_len, context_limit=args.c_len).build()
+    model.load_weights(args.model_path)
+
+    test_dataset = SquadReader(args.test_path)
+    tokenizer = get_tokenizer(lower=args.lower, as_str=False)
+    converter = SquadEvalConverter(token_to_index, PAD_TOKEN, UNK_TOKEN, tokenizer,
+                                   question_max_len=args.q_len, context_max_len=args.c_len)
+    test_generator = Iterator(test_dataset, args.batch, converter, False, False)
+    predictions = {}
+    for inputs, id_ in test_generator:
+        _, _, start_indices, end_indices = model.predict_on_batch(inputs)
+
+        context = inputs[1]
+        for i, (start, end) in enumerate(zip(start_indices, end_indices)):
+            prediction = ' '.join(index_to_token[context[i][j]] for j in range(start, end + 1))
+            predictions[id_[i]] = prediction
+
+    with open('predictions.json', 'w') as f:
+        json.dump(predictions, f, indent=2)
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--batch', default=32, type=int)
+    parser.add_argument('--embed', default=300, type=int)
+    parser.add_argument('--hidden', default=300, type=int)
+    parser.add_argument('--test-path', default='./data/dev-v1.1.txt', type=str)
+    parser.add_argument('--vocab-file', default='./data/vocab_question_context_min-freq10_max_size.pkl', type=str)
+    parser.add_argument('--lower', default=False, action='store_true')
+    parser.add_argument('--q-len', default=50, type=int)
+    parser.add_argument('--c-len', default=650, type=int)
+    parser.add_argument('--model-path', type=str)
+    args = parser.parse_args()
+    main(args)
