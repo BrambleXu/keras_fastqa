a7c6d2185f348f9c8323dcf6e3c5fa400f033bc7
yasufumi <yasufumi.taniguchi@gmail.com>
2018-08-20 14:54:10 +0900

    add an option to predict_answers.py

--- predict_answers.py
+++ predict_answers.py
@@ -12,7 +12,8 @@ def main(args):
     token_to_index, _ = Vocabulary.load(args.vocab_file)
 
     model = FastQA(len(token_to_index), args.embed, args.hidden,
-                   question_limit=args.q_len, context_limit=args.c_len).build()
+                   question_limit=args.q_len, context_limit=args.c_len,
+                   with_feature=not args.without_feature).build()
     model.load_weights(args.model_path)
 
     test_dataset = SquadReader(args.test_path)
@@ -43,6 +44,7 @@ if __name__ == '__main__':
     parser.add_argument('--test-path', default='./data/dev-v1.1.txt', type=str)
     parser.add_argument('--vocab-file', default='./data/vocab_question_context_min-freq10_max_size.pkl', type=str)
     parser.add_argument('--lower', default=False, action='store_true')
+    parser.add_argument('--without-feature', default=False, action='store_true')
     parser.add_argument('--q-len', default=50, type=int)
     parser.add_argument('--c-len', default=650, type=int)
     parser.add_argument('--model-path', type=str)
